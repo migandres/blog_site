3fd8f0b6bbf5f9f3b008151627af1f77092305f9
codicattus <codicattus@gmail.com>
2025-10-20 14:18:31 +0000

    Creando model managers

--- src/blog/models.py
+++ src/blog/models.py
@@ -2,6 +2,10 @@ from django.db import models
 from django.utils import timezone
 from django.conf import settings
 
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(status=Post.Status.PUBLISHED)
+
 class Post(models.Model):
     class Status(models.TextChoices):
         DRAFT = 'DF', 'Draft'
@@ -23,9 +27,12 @@ class Post(models.Model):
         on_delete=models.CASCADE,
         related_name='blog_posts'
     )
+    objects = models.Manager()  
+    published = PublishedManager() 
 
     class Meta:
         ordering = ('-publish',)
         indexes = [models.Index(fields=['-publish'])]
     def __str__(self):
         return self.title
+
