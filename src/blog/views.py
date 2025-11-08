e1789d72c8a41d242ae65c4562c802fa625e3b2a
codicattus <codicattus@gmail.com>
2025-11-08 10:38:02 +0000

    Viendo las vistas basadas en clases

--- src/blog/views.py
+++ src/blog/views.py
@@ -1,13 +1,30 @@
 from django.core.paginator import Paginator
 from django.shortcuts import get_object_or_404, render
 from .models import Post
+from django.views.generic import ListView
 
-def post_list(request):
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
+
+    def paginate_queryset(self, queryset, page_size):
+        paginator = Paginator(queryset, page_size)
+        page = self.request.GET.get('page')
+        return (
+            paginator, 
+            paginator.get_page(page),
+            paginator.get_page(page).object_list,
+            paginator.get_page(page).has_other_pages(),
+        )
+
+""" def post_list(request):
     posts = Post.published.all()
     paginator = Paginator(posts, 3)
     page_number = request.GET.get('page', 1)
     posts = paginator.get_page(page_number)
-    return render(request, 'blog/post/list.html', {'posts': posts})
+    return render(request, 'blog/post/list.html', {'posts': posts}) """
 
 def post_detail(request, post):
     post = get_object_or_404(Post, slug=post, status=Post.Status.PUBLISHED)
