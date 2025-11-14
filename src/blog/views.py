d3743cc15c895e3c66176a43433cd4de6c0a1e00
codicattus <codicattus@gmail.com>
2025-11-14 17:55:57 +0000

    Añadiendo comentarios - 1

--- src/blog/views.py
+++ src/blog/views.py
@@ -1,9 +1,10 @@
 from django.core.paginator import Paginator
 from django.shortcuts import get_object_or_404, render
-from .models import Post
 from django.views.generic import ListView
-from .forms import EmailPostForm
+from django.views.decorators.http import require_POST 
 from django.core.mail import send_mail
+from .models import Post
+from .forms import EmailPostForm, CommentForm
 
 class PostListView(ListView):
     queryset = Post.published.all()
@@ -64,3 +65,53 @@ def post_share(request, post_id):
     else:
         form = EmailPostForm()
     return render(request, 'blog/post/share.html', { 'post': post, 'form': form, 'sent': sent })
+
+@require_POST
+def post_comment(request, post_id):
+    post = get_object_or_404(
+        Post,
+        id = post_id,
+        status = Post.Status.PUBLISHED
+    )
+    comment = None
+    form = CommentForm(data = request.POST)
+    if form.is_valid():
+        comment = form.save(commit=False)
+        comment.post = post
+        comment.save()
+    return render(
+        request,
+        'blog/post/comment.html',
+        {
+            'post': post,
+            'form': form,
+            'comment': comment
+        }
+    )
+
+@require_POST
+def post_comment(request, post_id):
+    post = get_object_or_404(
+        Post, 
+        id=post_id, 
+        status=Post.Status.PUBLISHED
+    )
+    comment = None
+    form = CommentForm(request.POST)
+
+    if form.is_valid():
+        comment = Comment(
+            post=post,
+            name=form.cleaned_data['name'],
+            email=form.cleaned_data['email'],
+            body=form.cleaned_data['body']
+        )
+        comment.save()
+    return render(
+        request, 
+        'blog/post/comment.html', {
+        'post': post,
+        'form': form,
+        'comment': comment
+        }
+    )
