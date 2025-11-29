cda27003bb6a8860612b8657a3e2bc9491b3c2a3
migandres <migandgon@gmail.com>
2025-11-29 12:03:20 +0000

    Creación del sitemap del sitio.

--- src/mysite/urls.py
+++ src/mysite/urls.py
@@ -15,9 +15,20 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.contrib.sitemaps.views import sitemap
 from django.urls import include, path
+
+from blog.sitemaps import postSitemap
+
+sitemaps = {'post':postSitemap,}
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('blog/', include('blog.urls', namespace='blog')),
+    path(
+        'sitemap.xml',
+         sitemap,
+         {'sitemaps':sitemaps },
+         name='django.contrib.sitemaps.views.sitemap'
+        )
 ]
