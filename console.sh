5241cd71c405d417034c6a9f612d71738b34b0fa
migandres <migandgon@gmail.com>
2025-12-16 17:02:53 +0000

    Migrando a PostgreSQL

---
+++ console.sh
@@ -0,0 +1,8 @@
+$ pip install psycopg2-binary
+Defaulting to user installation because normal site-packages is not writeable
+Collecting psycopg2-binary
+  Downloading psycopg2_binary-2.9.11-cp313-cp313-manylinux2014_x86_64.manylinux_2_17_x86_64.whl.metadata (4.9 kB)
+Downloading psycopg2_binary-2.9.11-cp313-cp313-manylinux2014_x86_64.manylinux_2_17_x86_64.whl (4.2 MB)
+   ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ 4.2/4.2 MB 17.6 MB/s  0:00:00
+Installing collected packages: psycopg2-binary
+Successfully installed psycopg2-binary-2.9.11
