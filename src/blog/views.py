90011e1805687a1bccc6729da238f162012d727f
migandres <migandgon@gmail.com>
2025-12-17 11:56:55 +0000

    Añadiendo búsquedas

--- src/blog/views.py
+++ src/blog/views.py
@@ -4,11 +4,12 @@ from django.views.generic import ListView
 from django.views.decorators.http import require_POST 
 from django.core.mail import send_mail
 from django.db.models import Count
+from django.contrib.postgres.search import SearchVector
 
 from taggit.models import Tag
 
 from .models import Post
-from .forms import EmailPostForm, CommentForm
+from .forms import EmailPostForm, CommentForm, SearchForm
 
 class PostListView(ListView):
     queryset = Post.published.all()
@@ -146,3 +147,26 @@ def post_comment(request, post_id):
 #         'comment': comment
 #         }
 #    )
+
+def post_search(request):
+    form = SearchForm()
+    query = None
+    results = []
+    if 'query' in request.GET:
+        form = SearchForm(request.GET)
+        if form.is_valid():
+            query = form.cleaned_data['query']
+            results = (
+                Post.published.annotate(
+                    search=SearchVector('title', 'body', config='simple'),
+                    ).filter(search=query)
+                )
+    return render(
+        request,
+        'blog/post/search.html',
+        {
+            'form': form,
+            'query': query,
+            'results': results
+        }
+    )
