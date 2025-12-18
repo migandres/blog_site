9992083f86b988709cda531bbdb055e7f8ef1dbb
migandres <migandgon@gmail.com>
2025-12-18 08:58:21 +0000

    Búsqueda por trigram

--- src/blog/views.py
+++ src/blog/views.py
@@ -5,6 +5,7 @@ from django.views.decorators.http import require_POST
 from django.core.mail import send_mail
 from django.db.models import Count
 from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank
+from django.contrib.postgres.search import TrigramSimilarity
 
 from taggit.models import Tag
 
@@ -156,14 +157,19 @@ def post_search(request):
         form = SearchForm(request.GET)
         if form.is_valid():
             query = form.cleaned_data['query']
-            search_vector = SearchVector('title', weight='A') + SearchVector('body', weight='B', config='simple')
-            search_query = SearchQuery(query)
+            # search_vector = SearchVector('title', weight='A') + SearchVector('body', weight='B', config='simple')
+            # search_query = SearchQuery(query)
+            # results = (
+            #     Post.published.annotate(
+            #         search = search_vector, 
+            #         rank =  SearchRank(search_vector, search_query)
+            #         ).filter(rank__gte=0.3).order_by('-rank')
+            # )
             results = (
                 Post.published.annotate(
-                    search = search_vector, 
-                    rank =  SearchRank(search_vector, search_query)
-                    ).filter(rank__gte=0.3).order_by('-rank')
-            )
+                    similarity = TrigramSimilarity('title', query),
+                    ).filter(similarity__gt=0.03).order_by('-similarity')
+                )
     return render(
         request,
         'blog/post/search.html',
